7bccb302f61a2e6b79d5f98e9a9fbe5185d697ea
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-07-08 09:10:55 +0800

    (adjust): Rename the example of progress bar and format the output

--- examples/progress.rs
+++ examples/progress.rs
@@ -4,17 +4,22 @@ use kdam::{tqdm, BarExt};
 use std::io::{stderr, IsTerminal, Result};
 
 fn main() -> Result<()> {
-    kdam::term::init(stderr().is_terminal());
     let mut pb = tqdm!(
-        desc = "Training",
         colour = "orangered",
         dynamic_ncols = true,
         force_refresh = true,
         total = 300,
         unit = "steps",
-        bar_format = "{desc suffix=''} | {percentage:.0}% = {count}/{total} {unit} | {rate:.1} {unit}/s | {remaining human=true} |{animation}|"
+        bar_format = "Training {desc suffix=''} ┃ \
+        {percentage:.0}% = {count}/{total} {unit} ┃ \
+        {rate:.1} {unit}/s ┃ \
+        {remaining human=true} \
+        ┃{animation}┃"
     );
 
+    pb.set_description("on x1234 items");
+
+    kdam::term::init(stderr().is_terminal());
     for _ in 0..300 {
         std::thread::sleep(std::time::Duration::from_secs_f32(0.025));
         pb.update(1)?;
@@ -22,7 +27,10 @@ fn main() -> Result<()> {
 
     pb.clear()?;
     pb.set_bar_format(
-        "{desc suffix=''} | {total} {unit} | {rate:.1} {unit}/s | {elapsed human=true}",
+        "Trained {desc suffix=''} ┃ \
+        {total} {unit} ┃ \
+        {rate:.1} {unit}/s ┃ \
+        {elapsed human=true}",
     )
     .unwrap();
     pb.refresh()?;
